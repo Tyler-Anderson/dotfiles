9caa26796f46dfda42225525d5ca629de1ced618
Tyler <tyler@brava.do>
2012-11-23 17:22:32 -0800

    have to install to home dir atm

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 DIR = `pwd`
-git clone https://github.com/Tyler-Anderson/dotfiles.git
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 mv ~/.vimrc ~/.vimrc_OLD
 mv ~/.screenrc ~/.screenrc_OLD
 mv ~/.zshrc ~/.zshrc_OLD
-ln -s $DIR/dotfiles/vimrc ~/.vimrc
-ln -s $DIR/dotfiles/screenrc ~/.screenrc
-ln -s $DIR/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/screenrc ~/.screenrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
 echo "aheuhaeuaheuhe yay"
