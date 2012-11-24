ad33ba384897add98c91a933d34e10725bfb91e6
Tyler <tyler@brava.do>
2012-11-23 17:18:44 -0800

    k

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DIR = `pwd`
+git clone https://github.com/Tyler-Anderson/dotfiles.git
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 mv ~/.vimrc ~/.vimrc_OLD
